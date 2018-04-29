34551b8689a4fffda34678e0fab4d3a3cc152eea
nav0t <31127422+nav0t@users.noreply.github.com>
2018-04-29 15:06:15 +0200

    Redone Folder Structure

---
+++ Sources/ControlSurfaceToggle/CSToggleController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ControlSurfaceToggle {
+
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    class CSToggleController : MonoBehaviour{
+
+        bool wasInAtmo = true;
+
+		void Start() {
+			if (FlightGlobals.ActiveVessel == null) {
+				Debug.LogError("ToggleController Start method; ActiveVessel still Null");
+			}
+			wasInAtmo = FlightGlobals.ActiveVessel.atmDensity != 0;
+			GameEvents.onVesselSituationChange.Add(UpdateCurrentAtmosphereState);
+
+		}
+
+		public void UpdateCurrentAtmosphereState(GameEvents.HostedFromToAction<Vessel, Vessel.Situations> data) {
+
+			if (FlightGlobals.ActiveVessel == null) {
+                return;
+            }
+
+            bool inAtmo = FlightGlobals.ActiveVessel.atmDensity != 0;
+
+            if (wasInAtmo && !inAtmo) {
+                ModuleCSToggle.disableGlobal(true);
+            }
+            if (!wasInAtmo && inAtmo) {
+                ModuleCSToggle.enableGlobal(true);
+            }
+            wasInAtmo = inAtmo;
+        }
+
+    }
+}
